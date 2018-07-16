968c4ae768533711cb24e01d8600a80a2ec2d3ea
Corné Plooy <corne@bitonic.nl>
2018-07-16 15:59:30 +0200

    Add docstrings.

--- storage.py
+++ storage.py
@@ -27,12 +27,29 @@ class Transaction(Struct):
 
 
 class Storage:
+	'''
+	BL4P data storage and business logic back-end.
+	This is a dummy class with only volatile (internal memory) storage.
+	An actual implementation should use something like SQL with
+	atomic database transactions.
+	'''
+
 	def __init__(self):
 		self.users = {}
 		self.transactions = {}
 
 
 	def startTransaction(self, receiver_userid, amount, timeDelta):
+		'''
+		Start a new transaction.
+
+		:param receiver_userid: the user ID of the receiver
+		:param amount: the amount to be transfered from sender to receiver
+		:param timeDelta: the maximum time for the sender to respond, in seconds
+
+		:returns: the payment hash
+		'''
+
 		#Just check that the user exists
 		receiver = self.users[receiver_userid]
 		assert receiver.id == receiver_userid
@@ -54,12 +71,28 @@ class Storage:
 
 
 	def processTimeout(self, paymentHash):
+		'''
+		Process a transaction time-out event.
+
+		:param paymentHash: the payment hash of the transaction
+		'''
+
 		tx = self.transactions[paymentHash]
 		if tx.status == TransactionStatus.waiting_for_sender:
 			tx.status = TransactionStatus.timeout
 
 
 	def processSenderAck(self, sender_userid, amount, paymentHash):
+		'''
+		Process acknowledgement by the sender.
+
+		:param sender_userid: the user ID of the sender
+		:param amount: the amount to be transfered from sender to receiver
+		:param paymentHash: the payment hash
+
+		:returns: the payment preimage
+		'''
+
 		sender = self.users[sender_userid]
 		assert sender.id == sender_userid
 
@@ -76,6 +109,12 @@ class Storage:
 
 
 	def processReceiverClaim(self, preimage):
+		'''
+		Process transaction claim by the receiver.
+
+		:param preimage: the payment preimage
+		'''
+
 		paymentHash = sha256(preimage)
 		tx = self.transactions[paymentHash]
 		receiver = self.users[tx.receiver_userid]
