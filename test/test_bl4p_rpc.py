bdfcf37167e1857fc75495ad24bd1a24aee79482
Corné Plooy <corne@bitonic.nl>
2018-08-08 13:06:08 +0200

    Update the bl4p_rpc unit test to the new interface

--- test/test_bl4p_rpc.py
+++ test/test_bl4p_rpc.py
@@ -4,18 +4,19 @@ from unittest.mock import patch, Mock
 
 sys.path.append('..')
 
+from api import bl4p_proto_pb2
 import bl4p_rpc
 
 
 
-class MockRPCServer:
+class MockServer:
 	def __init__(self):
 		self.RPCFunctions = {}
 		self.timeoutFunctions = []
 
 
-	def registerRPCFunction(self, name, function, argsDef):
-		self.RPCFunctions[name] = function, argsDef
+	def registerRPCFunction(self, requestType, function):
+		self.RPCFunctions[requestType] = function
 
 
 	def registerTimeoutFunction(self, function):
@@ -49,17 +50,17 @@ class TestBL4PRPC(unittest.TestCase):
 	@patch('bl4p_rpc.start'    , return_value=100)
 	@patch('bl4p_rpc.send'     , return_value=101)
 	@patch('bl4p_rpc.receive'  , return_value=102)
-	@patch('bl4p_rpc.getstatus', return_value=103)
-	def test_registerRPC(self, mock_getstatus, mock_receive, mock_send, mock_start):
+	@patch('bl4p_rpc.getStatus', return_value=103)
+	def test_registerRPC(self, mock_getStatus, mock_receive, mock_send, mock_start):
 		mocks = \
 		{
-		'start'    : mock_start,
-		'send'     : mock_send,
-		'receive'  : mock_receive,
-		'getstatus': mock_getstatus,
+		bl4p_proto_pb2.BL4P_Start    : mock_start,
+		bl4p_proto_pb2.BL4P_Send     : mock_send,
+		bl4p_proto_pb2.BL4P_Receive  : mock_receive,
+		bl4p_proto_pb2.BL4P_GetStatus: mock_getStatus,
 		}
 
-		server = MockRPCServer()
+		server = MockServer()
 		storage = Mock()
 
 		bl4p_rpc.registerRPC(server, storage)
@@ -67,74 +68,40 @@ class TestBL4PRPC(unittest.TestCase):
 		self.assertEqual(len(server.RPCFunctions.keys()), len(mocks))
 
 		#Test the functions passed to registerRPCFunction
-		for name in mocks.keys():
-			registeredFunction = server.RPCFunctions[name][0]
+		for requestType in mocks.keys():
+			registeredFunction = server.RPCFunctions[requestType]
 			ret = registeredFunction(1,2,3)
-			for mockname, mock in mocks.items():
-				if mockname == name:
+			for mockType, mock in mocks.items():
+				if mockType == requestType:
 					mock.assert_called_once_with(storage, 1, 2, 3)
 					self.assertEqual(ret, mock.return_value)
 				else:
 					mock.assert_not_called()
 				mock.reset_mock()
 
-		#Test the argsDef passed to registerRPCFunction
-		self.assertEqual(server.RPCFunctions['start'][1],
-			(('userid', int), ('amount', int), ('timedelta', float), ('receiverpaysfee', bl4p_rpc.str2bool))
-			)
-		self.assertEqual(server.RPCFunctions['send'][1],
-			(('userid', int), ('amount', int), ('paymenthash', bl4p_rpc.hex2binary))
-			)
-		self.assertEqual(server.RPCFunctions['receive'][1],
-			(('paymentpreimage', bl4p_rpc.hex2binary), )
-			)
-		self.assertEqual(server.RPCFunctions['getstatus'][1],
-			(('userid', int), ('paymenthash', bl4p_rpc.hex2binary))
-			)
-
-		self.assertEqual(server.timeoutFunctions, [storage.processTimeouts])
-
-
-	def test_str2bool(self):
-		self.assertEqual(bl4p_rpc.str2bool('True'), True)
-		self.assertEqual(bl4p_rpc.str2bool('TRUE'), True)
-		self.assertEqual(bl4p_rpc.str2bool('true'), True)
-
-		self.assertEqual(bl4p_rpc.str2bool('False'), False)
-		self.assertEqual(bl4p_rpc.str2bool('FALSE'), False)
-		self.assertEqual(bl4p_rpc.str2bool('false'), False)
-
-		for s in ['', '0', '1', 'foo']:
-			with self.assertRaises(ValueError):
-				bl4p_rpc.str2bool(s)
-
-
-	def test_hex2binary(self):
-		self.assertEqual(bl4p_rpc.hex2binary(''), b'')
-		self.assertEqual(bl4p_rpc.hex2binary('00'), b'\x00')
-		self.assertEqual(bl4p_rpc.hex2binary('01'), b'\x01')
-		self.assertEqual(bl4p_rpc.hex2binary('ff'), b'\xff')
-		self.assertEqual(bl4p_rpc.hex2binary('1234'), b'\x12\x34')
-		self.assertEqual(bl4p_rpc.hex2binary('0123456789abcdef'), b'\x01\x23\x45\x67\x89\xab\xcd\xef')
-
-		for s in ['0', 'aaa', 'x0', '00x']:
-			with self.assertRaises(ValueError):
-				bl4p_rpc.hex2binary(s)
+		#TODO
+		#self.assertEqual(server.timeoutFunctions, [storage.processTimeouts])
 
 
 	def test_start(self):
 		storage = MockStorage()
 
+		request = Mock()
+		request.amount.amount = 5
+		request.sender_timeout_delta_ms = 6
+		request.receiver_pays_fee = 7
+
 		#Successfull call
 		storage.startTransaction = Mock(
 			return_value=(100, 99, b'\x00\xff')
 			)
-		ret = bl4p_rpc.start(storage, userid=4, amount=5, timedelta=6, receiverpaysfee=7)
-		self.assertEqual(ret,
-			{'senderamount': 100, 'receiveramount': 99, 'paymenthash': '00ff'}
-			)
+		result = bl4p_rpc.start(storage, userID=4, request=request)
+		self.assertTrue(isinstance(result, bl4p_proto_pb2.BL4P_StartResult))
+		self.assertEqual(result.sender_amount.amount, 100)
+		self.assertEqual(result.receiver_amount.amount, 99)
+		self.assertEqual(result.payment_hash.data, b'\x00\xff')
 		storage.startTransaction.assert_called_once_with(
-			receiver_userid=4, amount=5, timeDelta=6, receiverPaysFee=7
+			receiver_userid=4, amount=5, timeDelta=6/1000.0, receiverPaysFee=7
 			)
 
 		#Exceptions
@@ -142,20 +109,23 @@ class TestBL4PRPC(unittest.TestCase):
 			storage.startTransaction.reset_mock()
 			storage.startTransaction.side_effect=xc
 			with self.assertRaises(Exception):
-				bl4p_rpc.start(storage, userid=4, amount=5, timedelta=6, receiverpaysfee=7)
+				bl4p_rpc.start(storage, userID=4, request=request)
 
 
 	def test_send(self):
 		storage = MockStorage()
 
+		request = Mock()
+		request.sender_amount.amount = 5
+		request.payment_hash.data = 6
+
 		#Successfull call
 		storage.processSenderAck = Mock(
 			return_value=b'\x00\xff'
 			)
-		ret = bl4p_rpc.send(storage, userid=4, amount=5, paymenthash=6)
-		self.assertEqual(ret,
-			{'paymentpreimage': '00ff'}
-			)
+		result = bl4p_rpc.send(storage, userID=4, request=request)
+		self.assertTrue(isinstance(result, bl4p_proto_pb2.BL4P_SendResult))
+		self.assertEqual(result.payment_preimage.data, b'\x00\xff')
 		storage.processSenderAck.assert_called_once_with(
 			sender_userid=4, amount=5, paymentHash=6
 			)
@@ -165,20 +135,21 @@ class TestBL4PRPC(unittest.TestCase):
 			storage.processSenderAck.reset_mock()
 			storage.processSenderAck.side_effect=xc
 			with self.assertRaises(Exception):
-				bl4p_rpc.send(storage, userid=4, amount=5, paymenthash=6)
+				bl4p_rpc.send(storage, userID=4, request=request)
 
 
 	def test_receive(self):
 		storage = MockStorage()
 
+		request = Mock()
+		request.payment_preimage.data = 5
+
 		#Successfull call
 		storage.processReceiverClaim = Mock()
-		ret = bl4p_rpc.receive(storage, paymentpreimage=4)
-		self.assertEqual(ret,
-			{}
-			)
+		result = bl4p_rpc.receive(storage, userID=4, request=request)
+		self.assertTrue(isinstance(result, bl4p_proto_pb2.BL4P_ReceiveResult))
 		storage.processReceiverClaim.assert_called_once_with(
-			paymentPreimage=4
+			paymentPreimage=5
 			)
 
 		#Exceptions
@@ -186,20 +157,22 @@ class TestBL4PRPC(unittest.TestCase):
 			storage.processReceiverClaim.reset_mock()
 			storage.processReceiverClaim.side_effect=xc
 			with self.assertRaises(Exception):
-				bl4p_rpc.receive(storage, paymentpreimage=4)
+				bl4p_rpc.receive(storage, request=request)
 
 
-	def test_getstatus(self):
+	def test_getStatus(self):
 		storage = MockStorage()
+
+		request = Mock()
+		request.payment_hash.data = 5
 
 		#Successfull call
 		storage.getTransactionStatus = Mock(
-			return_value='fubar'
+			return_value='completed'
 			)
-		ret = bl4p_rpc.getstatus(storage, userid=4, paymenthash=5)
-		self.assertEqual(ret,
-			{'status': 'fubar'}
-			)
+		result = bl4p_rpc.getStatus(storage, userID=4, request=request)
+		self.assertTrue(isinstance(result, bl4p_proto_pb2.BL4P_GetStatusResult))
+		self.assertEqual(result.status, bl4p_proto_pb2._completed)
 		storage.getTransactionStatus.assert_called_once_with(
 			userid=4, paymentHash=5
 			)
@@ -209,7 +182,7 @@ class TestBL4PRPC(unittest.TestCase):
 			storage.getTransactionStatus.reset_mock()
 			storage.getTransactionStatus.side_effect=xc
 			with self.assertRaises(Exception):
-				bl4p_rpc.getstatus(storage, userid=4, paymenthash=5)
+				bl4p_rpc.getstatus(storage, userid=4, request=request)
 
 
 
