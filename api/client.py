7e4c9f9bbd0a2380fe248d90b761164cf41a426e
Corné Plooy <corne@bitonic.nl>
2018-09-13 11:46:39 +0200

    Add license headers to the API source files

--- api/client.py
+++ api/client.py
@@ -1,3 +1,20 @@
+#    Copyright (C) 2018 by Bitonic B.V.
+#
+#    This file is part of the BL4P API.
+#
+#    The BL4P API is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The BL4P API is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with the BL4P API. If not, see <http://www.gnu.org/licenses/>.
+
 import websocket
 
 from . import bl4p_pb2
