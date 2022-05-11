c209cdb58643fa141d98419ab01fc69845373ec2
Corné Plooy <corne@bitonic.nl>
2022-05-11 11:02:59 +0200

    Add copyright headers

--- server.py
+++ server.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#    Copyright (C) 2021 by Bitonic B.V.
+#
+#    This file is part of the BL4P Server.
+#
+#    The BL4P Server is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The BL4P Server is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with the BL4P Server. If not, see <http://www.gnu.org/licenses/>.
 
 from bl4p_server import __main__
 
--- test/test_rpcserver.py
+++ test/test_rpcserver.py
@@ -1,3 +1,20 @@
+#    Copyright (C) 2018-2021 by Bitonic B.V.
+#
+#    This file is part of the BL4P Server.
+#
+#    The BL4P Server is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The BL4P Server is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with the BL4P Server. If not, see <http://www.gnu.org/licenses/>.
+
 import asyncio
 import sys
 import threading
